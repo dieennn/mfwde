8563881c09cb46a7d070d4c4e8cc7167d2c1e345
Dien <dien_kamal@ymail.com>
2021-07-06 21:41:16 +0700

    PWA: Konfigurasi Drawer

--- PWA/movie-catalogue/src/scripts/index.js
+++ PWA/movie-catalogue/src/scripts/index.js
@@ -1,5 +1,10 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
-// eslint-disable-next-line no-console
-console.log('Hello Coders!');
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
