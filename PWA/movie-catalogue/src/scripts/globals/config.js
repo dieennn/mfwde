f7e62934e2d087e559a35fd5c1cafd0db62397cd
Dien <dien_kamal@ymail.com>
2021-07-07 01:18:41 +0700

    PWA: Latihan: Notifikasi https://github.com/dicodingacademy/a219-mfwde-labs/tree/506-movie-catalogue-notification

--- PWA/movie-catalogue/src/scripts/globals/config.js
+++ PWA/movie-catalogue/src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   KEY: '6556eaa1b1a2430940d6ad5e6cf9e5d0',
   BASE_URL: 'https://api.themoviedb.org/3/',
-  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'movie-catalogue-database',
