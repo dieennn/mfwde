1b6ce226602ef3faf1b37f472be867c3a15476b3
Dien <dien_kamal@ymail.com>
2021-07-14 22:09:42 +0700

    Rahasia Seorang Mata-Mata

--- tdd/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ tdd/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -13,9 +13,14 @@ class FavoriteMovieSearchPresenter {
     });
   }
 
-  _searchMovies(latestQuery) {
+  async _searchMovies(latestQuery) {
     this._latestQuery = latestQuery;
-    this._favoriteMovies.searchMovies(this.latestQuery);
+
+    const foundMovies = await this._favoriteMovies.searchMovies(
+      this.latestQuery
+    );
+
+    this._showFoundMovies(foundMovies);
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -31,6 +36,10 @@ class FavoriteMovieSearchPresenter {
     );
 
     document.querySelector('.movies').innerHTML = html;
+
+    document
+      .getElementById('movie-search-container')
+      .dispatchEvent(new Event('movies:searched:updated'));
   }
 
   get latestQuery() {
