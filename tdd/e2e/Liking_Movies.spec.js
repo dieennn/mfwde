4eafc3678397062dc124f033ecf826b63f40c53c
Dien <dien_kamal@ymail.com>
2021-07-14 23:18:56 +0700

    Jangan Sampai Salah Nama

--- tdd/e2e/Liking_Movies.spec.js
+++ tdd/e2e/Liking_Movies.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Movies');
 
 Before((I) => {
@@ -17,17 +19,23 @@ Scenario('liking one movie', (I) => {
   // … kita akan mengisi uji coba berikutnya …
 });
 
-Scenario('liking one movie', (I) => {
+Scenario('liking one movie', async ({ I }) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 
   I.amOnPage('/');
 
   I.seeElement('.movie__title a');
-  I.click(locate('.movie__title a').first());
+
+  const firstFilm = locate('.movie__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
   I.seeElement('.movie-item');
+  const likedFilmTitle = await I.grabTextFrom('.movie__title');
+
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
 });
